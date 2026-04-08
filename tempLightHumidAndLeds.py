bc3353e65add0956d0aa3e7f37785f1b8ef80c0d
Jipsey-A <134132487+Jipsey-A@users.noreply.github.com>
2026-04-08 07:58:10 +0100

    Add files via upload

---
+++ tempLightHumidAndLeds.py
@@ -0,0 +1,116 @@
+import sys, math
+#sys allows us to check we're on a Pico. math is needed for temperature calculations
+from time import sleep
+#sleep allows us to slow things down with a pause after each round
+from machine import ADC, Pin, PWM
+#ADC and Pin tell the code where to look for hardware
+
+#preset thresholds
+tempThreshold = 20 #degrees C
+lightThreshold = 50 #percent
+humidityThreshold = 30 #percent
+
+#setup hardware
+#sensors
+tempLevel = ADC(27) # thermister as a voltage divider into pin 27
+lightLevel = ADC(26) # Photodiode as a voltage divider into pin 26
+humidityLevel = ADC(28) # humidity sensor on pin 28
+humidityDrive = PWM(11) #humidity sensor is driven by a pulse from pin 11
+humidityDrive.freq(1000) #this sets the frequency of the pulse to 1kHz
+humidityDrive.duty_u16(32767) #this sets PWM to be on 50% (32767/65535)
+
+#actuators
+redLed = Pin(15, Pin.OUT) #red LED into pin 15 responds to temp
+yellowLed = Pin(14, Pin.OUT) #yellow LED into pin 14 responds to light level
+blueLed = Pin(13, Pin.OUT) #blue LED into pin 14 responds to humidity level
+
+#Check it's running on a pico
+def picocheck():
+    assert('rp2' in sys.platform), "function for rasp Pico only"
+    print("Raspberry pico confirmed")
+
+#read thermistor, calibrate and convert to degrees C
+def readTemp(adc_value):
+    Vout = (3.3/65535)*(adc_value)
+    # Steinhart Constants as set by hardware datasheet
+    A = -0.00212447066499
+
+    B = 0.00072486479719
+
+    C = -0.00000150782459
+
+        # Calculate Resistance
+    Vin = 3.3
+    Ro = 10200 #this is the size of resistor in voltage divider circuit
+    try:
+        Rt = (Vout * Ro) / (Vin - Vout)
+    except:
+        Rt = 200 #this is a work around in case Vin-Vout = o
+    
+    # Steinhart - Hart Equation
+    TempKelvin = 1 / (A + (B * math.log(Rt)) + (C * math.pow(math.log(Rt), 3)))
+
+    # Convert from Kelvin to Celsius
+    TempCelsius = TempKelvin - 273.15
+    return TempCelsius
+
+# Start of main loop
+#check  that it's running on the correct hardware
+try:
+    picocheck()
+except:
+    print("not running on a pico")
+
+
+while True:
+#start by reading the sensors
+    
+    #TEMPERATURE
+    #read the temperature from the thermistor
+    #use readTemp() to calibrate and convert to C. Rnd to 1 dp
+    #print the temp reading
+    tempC = round(readTemp(tempLevel.read_u16()),1)
+    print(f"Current temp is {tempC}C")
+    
+    #LIGHT LEVEL
+    #read the light level from photodiode
+    #convert to a percentage, 0 dp
+    #print the light level reading
+    lightPct = int(lightLevel.read_u16())/655.35
+    print(f"Light level is {lightPct}%")
+
+    #HUMIDITY LEVEL
+    #read the humidity level from sensor
+    #convert to a percentage, 0 dp, flip so 100% is most humid
+    #print the light level reading
+    humidityPct = int(100-(humidityLevel.read_u16())/655.35)
+    print(f"Relative humidity is {humidityPct}%")
+
+
+#now instruct the activator (in this case a red LED)
+#to respond depending on comparison of reading with threshold
+    #if tempC is less than temp threshold turn heater (represented here by red LED) on
+    #if tempC is not less than temp threshold then turn red LED off
+    redLed.value(tempC < tempThreshold)
+    if tempC < tempThreshold:
+        print("Temperature lower than threshold - turn heater (Red LED) on")
+    else:
+        print ("Temperature exceeds threshold - turn heater (Red LED) off")
+        
+    yellowLed.value(lightPct < lightThreshold)
+    if lightPct < lightThreshold:
+        print("Light is dimmer than threshold - turn main light (Yellow LED) on")
+    else:
+        print ("Light is brighter than threshold - turn main light (Yellow LED) off")
+        
+    blueLed.value(humidityPct > humidityThreshold)
+    if humidityPct < humidityThreshold:
+        print("Humidity is lower than threshold - turn extractor fan (Blue LED) off")
+    else:
+        print ("Humidity is higher than threshold - turn extractor fan (Yellow LED) on")
+        
+        
+#A pause to slow it all down a little before it returns to the start of this main loop.
+    sleep(1)
+    print("****")
+    
