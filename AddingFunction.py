3a01cb43162fd345c96bc8b1b6573fa2f80fc90d
Jipsey-A <134132487+Jipsey-A@users.noreply.github.com>
2026-04-10 16:12:15 +0100

    Add files via upload

---
+++ AddingFunction.py
@@ -0,0 +1,287 @@
+import sys, math
+#sys allows us to check we're on a Pico. math is needed for temperature calculations
+import time
+from time import sleep
+#sleep allows us to slow things down with a pause after each round
+from machine import ADC, Pin, PWM
+#ADC and Pin tell the code where to look for hardware
+import network #needed for wifi set-up
+from mqtt import MQTTClient #needed for mqtt
+
+#set-up MQTT info
+SSID = 'PLUSNET-3PC2PM' #home settings*
+PWD = 'GM67xEANVpT9gA'#*
+thingName = 'JennysThing' # change and make this unique. This is the address for this specific IoT device
+broker ="broker.emqx.io" #this is for the phone app.
+
+#set-up topics we're publishing to
+tempTopic = thingName+'/temperature'
+lightTopic = thingName+'/lightLevel'
+humidTopic = thingName+'/humidity'
+tempThresholdTopic = thingName + '/tempThreshold'
+lightThresholdTopic = thingName + '/lightThreshold'
+humidThresholdTopic = thingName + '/humidThreshold'
+tempActionTopic = thingName+'/tempAction'
+lightActionTopic = thingName+'/lightAction'
+humidActionTopic = thingName+'/humidAction'
+boostButtonTopic = thingName+'/boostButton'
+boostStatusTopic = thingName+'/boostStatus'
+
+#setup hardware
+#sensors & associated info
+tempLevel = ADC(27) # thermister as a voltage divider into pin 27
+lightLevel = ADC(26) # Photodiode as a voltage divider into pin 26
+humidityLevel = ADC(28) # humidity sensor on pin 28
+humidityDrive = PWM(11) #humidity sensor is driven by a pulse from pin 11
+humidityDrive.freq(1000) #this sets the frequency of the pulse to 1kHz
+humidityDrive.duty_u16(32767) #this sets PWM to be on 50% (32767/65535)
+heaterBoost = Pin(10, Pin.IN, Pin.PULL_UP) #this is a button
+boostActive = False #a boolean to keep tabs on and communicate the boost status
+boostEndTime = 0 #this updates in boostStart() to tell us what time the boost finishes
+lastSensorTime = 0 #this tells us how long it's been since the last sensor reading
+sensorInterval = 1000   # This controls the rate of readings. 1000 ms = 1 second
+light_action = 0 #set's up a boolean to control whether the yellow led is on or off
+boostTime = 5
+
+#actuators
+redLed = Pin(15, Pin.OUT) #red LED into pin 15 responds to temp
+yellowLed = Pin(14, Pin.OUT) #yellow LED into pin 14 responds to light level
+blueLed = Pin(13, Pin.OUT) #blue LED into pin 13 responds to humidity level
+
+#preset variables
+tempThreshold = 20 #degrees C
+lightThreshold = 50 #percent
+humidityThreshold = 15 #percent
+
+#Check it's running on a pico
+def picocheck():
+    assert('rp2' in sys.platform), "function for rasp Pico only"
+    print("Raspberry pico confirmed")
+
+# Connect to the wifi access point
+def wifi():
+    #sets up the wifi info and request a connection
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(SSID, PWD)
+    #Wait for connection
+    print("Connecting to wifi")
+    while not wlan.isconnected():
+        sleep(0.2)
+        print("connecting....")
+    #Once connected let me know
+    print("Connected to wifi, Ip address is: ", wlan.ifconfig()[0])
+    #Create MQTT client to send and receive
+    client = MQTTClient(thingName, broker, 1883
+    )
+    client.set_callback(masterCallback)
+    #connect to broker   
+    try:
+        client.connect()
+        print('Connected to MQTT broker successfully')
+    except:
+        print('Failed to connect to Broker')
+        raise
+    #Subscribe to topics - this tells it to look out for messages with this in it's topic section
+    client.subscribe(tempThresholdTopic)
+    client.subscribe(lightThresholdTopic)
+    client.subscribe(humidThresholdTopic)
+    client.subscribe(boostButtonTopic)
+    client.subscribe(lightActionTopic)
+    
+    return client
+
+#This tells the Pico what to do when it receives a message that it's subscribed to
+def masterCallback(topic, msg):
+    #make variables global so they are modified throughout the code
+    global tempThreshold, lightThreshold, humidityThreshold, light_action, boostActive
+
+#convert bytes to strings so they can be compared
+    topic = topic.decode()
+    msg = msg.decode()
+    
+#let's me know we've received a message and what it says
+    #print("Received:", topic, msg) commented out because too many messages from dashboard
+    
+#update thresholds accordingly & immediately. Uses 'try' and 'except' in case it receives something unexpected in the data field
+    if "tempThreshold" in topic:
+        try:
+            tempThreshold = int(msg)
+            print("New temperature threshold:", tempThreshold)
+        except:
+            print("Invalid temp threshold received")
+    elif "lightThreshold" in topic:
+        try:
+            lightThreshold = int(msg)
+            print("New light threshold:", lightThreshold)
+        except:
+            print("Invalid light threshold received")
+    elif "humidThreshold" in topic:
+        try:
+            humidityThreshold = int(msg)
+            print("New humidity threshold:", humidityThreshold)
+        except:
+            print("Invalid humidity threshold received")
+    elif "boostButton" in topic:
+        try:
+            if heaterBoost.value() == 1 and not boostActive:
+                print("Boost button on remote dashboard pressed")
+                boostStart()
+        except:
+            print("Invalid boost button request received")
+    elif "lightAction" in topic:
+        try:
+            light_action = int(msg)
+            #print("Dashboard set light action to:", light_action)  #commented out as too many messages from dashboard
+        except:
+            print("Invalid boost button request received")        
+
+
+#check if the physical button on the IoT device has been pressed
+#if so, make boostActive =1 and start boostStart bfunction
+def physicalButton():
+    global boostActive
+    if heaterBoost.value() == 0 and not boostActive:
+        print("Boost button on IoT device pressed")
+        boostStart()
+
+#start the heat boost sequence
+def boostStart():
+    global boostActive, boostEndTime, client, boostTime
+    if boostActive == False:
+        boostActive = True
+        boostEndTime = time.time() + boostTime
+        print("Heater Boost activated for 20 seconds")
+        client.publish(boostStatusTopic, str(boostActive))
+
+# Check if boost time has expired and handles flashing LED
+def boostStatusCheck():
+    global boostActive, boostEndTime, client
+    
+    #if boost time has ended make boostActive false
+    if boostActive and time.time() >= boostEndTime:
+        boostActive = False
+        print("Heater boost ended - returning to normal control")
+    #if boost is still active flash the LED
+    elif boostActive:
+        ms = time.ticks_ms()
+        redLed.value((ms // 250) % 2)   # 2 Hz flash
+
+#read thermistor, calibrate and convert to degrees C
+def readTemp(adc_value):
+    Vout = (3.3/65535)*(adc_value)
+    # Steinhart Constants as set by hardware datasheet
+    A = -0.00212447066499
+
+    B = 0.00072486479719
+
+    C = -0.00000150782459
+
+        # Calculate Resistance
+    Vin = 3.3
+    Ro = 10200 #this is the size of resistor in voltage divider circuit
+    try:
+        Rt = (Vout * Ro) / (Vin - Vout)
+    except:
+        Rt = 200 #this is a work around in case Vin-Vout = o
+    
+    # Steinhart - Hart Equation
+    TempKelvin = 1 / (A + (B * math.log(Rt)) + (C * math.pow(math.log(Rt), 3)))
+
+    # Convert from Kelvin to Celsius
+    TempCelsius = TempKelvin - 273.15
+    return TempCelsius
+
+# Start of main loop
+#check  that it's running on the correct hardware
+try:
+    picocheck()
+except:
+    print("not running on a pico")
+
+
+try:
+    client = wifi()
+except:
+    print("wifi not connecting")
+
+while True:
+    # This helps me to see when we return to this point in the code
+        
+    boostStatusCheck() #check if boost is in progress
+    physicalButton() #check if physical button has been pressed
+    
+    ms = time.ticks_ms() # reads the time
+    #only reads sensors and amends actuators if sensorInterval has passed.
+    if time.ticks_diff(ms, lastSensorTime) >= sensorInterval:
+        lastSensorTime = ms
+
+        print("****")     
+
+        #start by reading the sensors
+        #for each sensor:
+            #read the sensor
+            #convert to useful figure inc rounding to appropriate dp
+            #print the reading (useful for debugging!)
+            #send the reading out as a message via MQTT jennysThing/___Topic
+            #check if any messages received
+    
+        #TEMPERATURE - tempTopic
+        tempC = round(readTemp(tempLevel.read_u16()),1)
+        print(f"Current temp is {tempC}C")
+        client.publish(tempTopic, str(tempC))
+        client.check_msg()
+    
+        #LIGHT LEVEL - lightTopic
+        lightPct = int(lightLevel.read_u16())/655.35
+        print(f"Light level is {lightPct}%")
+        client.publish(lightTopic, str(int(lightPct)))
+        client.check_msg()    
+
+        #HUMIDITY LEVEL - humidTopic
+        humidityPct = int(100-(humidityLevel.read_u16())/655.35)
+        print(f"Relative humidity is {humidityPct}%")
+        client.publish(humidTopic, str(int(humidityPct)))
+        client.check_msg()
+    
+        #now instruct the activators to respond depending on comparison of reading with threshold
+        #and to publish a message communicating the status of the action.
+        
+        #TEMPERATURE - heater represented here by red LED. Turn on if tempC<threshold, otherwise off  
+        if boostActive:
+            temp_action = 1
+            print("Boost mode")
+        elif tempC < tempThreshold:
+            temp_action = 1
+            redLed.value(temp_action)
+            print("Temperature lower than threshold - turn heater (Red LED) on")
+        else:
+            temp_action = 0
+            redLed.value(temp_action)
+            print ("Temperature exceeds threshold - turn heater (Red LED) off")
+        
+        client.publish(tempActionTopic, str(temp_action))
+    
+        #LIGHT main light represented by yellow LED is controlled by Node red. 
+        yellowLed.value(light_action)
+        if light_action == 1:
+            print("Light is dimmer than threshold - turn main light (Yellow LED) on")
+        else:
+            print ("Light is brighter than threshold - turn main light (Yellow LED) off")
+        client.publish(lightActionTopic, str(light_action))
+
+
+        #HUMIDITY extractor represeted by blue LED. turn on if too humid    
+        if humidityPct > humidityThreshold:
+            humid_action = 1
+            blueLed.value(humid_action)        
+            print ("Humidity is higher than threshold - turn extractor fan (blue LED) on")
+        else:
+            humid_action = 0
+            blueLed.value(humid_action)
+            print("Humidity is lower than threshold - turn extractor fan (Blue LED) off")
+        client.publish(humidActionTopic, str(humid_action))
+        
+        #A pause to slow it all down a little before it returns to the start of this main loop.
+    sleep(0.01)
+    
